6b028dfe6fc29321399ff7f99846204d93c3ac0b
Abbi Parchment <abbigail151@gmail.com>
2021-10-21 18:08:07 -0500

    Modified Html file, Completed CSS and JS Files along with pictures used

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
 /* Add your JavaScript to this file */
+window.onload = function() {
+
+    var emailInput = document.getElementsByTagName("input")[0];
+    var message = document.getElementsByClassName("message")[0];
+    document.getElementsByClassName("btn")[1].onclick=function(e){
+
+        if (emailInput.value.length < 1){
+            message.innerHTML = "Please enter a valid email address.";
+        }
+        else {
+            message.innerHTML = `Thank you! Your email address ${emailInput.value} has been added to our mailing list!`;
+        }
+        e.preventDefault();
+    }
+}
